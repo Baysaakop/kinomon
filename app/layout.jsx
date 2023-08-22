614e8528cd64cd1d19ff66aec07104cd482b7ae0
Baysaakop <baysaakop@gmail.com>
2023-08-22 17:07:09 +0800

    Post  component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,9 @@
 import "@/styles/globals.css"
 import Nav from "./Nav"
-import { Roboto } from 'next/font/google'
- 
-const roboto = Roboto({
+import { Courier_Prime } from 'next/font/google'
+import QueryWrapper from './auth/QueryWrapper' 
+
+const courier_prime = Courier_Prime({
   weight: '400',
   subsets: ['latin'],
   display: 'swap',
@@ -10,11 +11,13 @@ const roboto = Roboto({
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en" className={roboto.className}>
+        <html lang="en" className={courier_prime.className}>
             <head />
             <body className="mx-4 md:mx-48 xl:mx-96 bg-gray-200">
-                <Nav />
-                {children}
+                <QueryWrapper>
+                    <Nav />
+                    {children}
+                </QueryWrapper>
             </body>
         </html>
     )
